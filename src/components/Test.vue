36159c288905de7b0009281faab3535935da4e0f
rayGreycloud <rayGreycloud@gmail.com>
2017-08-25 14:46:41 -0700

    Add demo component

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="test"><h1>Test</h1></div>
+  <div class="test">
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
+    <p v-else="showName">Friend</p>
+
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'test'
+    name: 'test',
+    data() {
+      return {
+        title: "Hello",
+        user: {
+          firstName: 'John',
+          lastName: 'Doe'
+        },
+        showName: false
+      }
+    }
   }
 </script>
 
