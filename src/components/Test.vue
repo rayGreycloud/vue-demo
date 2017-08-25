da10f36472d6dbe177a31a9565271b65c6e40de1
rayGreycloud <rayGreycloud@gmail.com>
2017-08-25 14:51:12 -0700

    Add example of for loop and connecting data input to display

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="test">
+    <input type="text" v-model="title"><br />
     <h1>{{title}}</h1>
     <p v-if="showName">{{user.firstName}}</p>
     <p v-else="showName">Friend</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
 
   </div>
 </template>
@@ -17,7 +21,12 @@
           firstName: 'John',
           lastName: 'Doe'
         },
-        showName: false
+        showName: true,
+        items: [
+          {title: 'Item One'},
+          {title: 'Item Two'},
+          {title: 'Item Three'}
+        ]
       }
     }
   }
